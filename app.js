0edea790f341f34ffc25d0887192f645f74398f4
Nam Khanh Nguyen <nguyenkhanhnam.13a@gmail.com>
2018-03-03 18:03:26 +0700

    Create movie UI

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+
+// My new require
+
+//For routing
+var movie = require('./routes/movie');
+
+
+
+
 var app = express();
 
 // view engine setup
@@ -24,6 +33,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+
+
+// Create routing
+app.use('/movie', movie);
+
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
